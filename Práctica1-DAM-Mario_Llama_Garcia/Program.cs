8055a8819faa1b3bb9107f2e37f4a58c2ad43ae5
UBU <UBU@CEE-AI-C11-S05>
2025-02-13 18:53:21 +0100

    Agregar archivos de proyecto.

---
+++ Práctica1-DAM-Mario_Llama_Garcia/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Práctica1_DAM_Mario_Llama_Garcia
+{
+    /// <summary>
+    /// Clase de ejemplo 1
+    /// </summary>
+    internal class Ejemplo1
+    {
+        /// <summary>
+        /// Este constructo iguala numero a n
+        /// </summary>
+        /// <param name="n"> n es el valor para numero</param>
+        public int numero;
+        public String texto;
+
+        public Ejemplo1(int n)
+        {
+            this.Funcion3();
+            numero = n;
+        }
+        /// <summary>
+        /// Este texto es de ejemplo de la función1
+        /// </summary>
+        /// <param name="txt"> recibe el texto</param>
+        /// 
+        public void Funcion1(String txt)
+        {
+            texto = txt;
+        }
+        /// <summary>
+        /// Texto de ejemplo
+        /// </summary>
+        /// <param name="n"> retorna un número</param>
+        public int Funcion2()
+        {
+            return numero;
+        }
+        /// <summary>
+        /// Texto de ejemplo
+        /// </summary>
+        /// <param name="n"> retorna un número</param>
+        
+        private String Funcion3()
+        {
+            return texto;
+        }
+
+        
+    }
+}
